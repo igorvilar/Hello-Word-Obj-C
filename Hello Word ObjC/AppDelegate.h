60ea1f857332ce883975c906b4c1c2ef0429f33b
trakimobile <igorvilar@outlook.com>
2016-04-28 10:26:09 -0300

    Add files project.

---
+++ Hello Word ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello Word ObjC
+//
+//  Created by Igor Vilar on 4/28/16.
+//  Copyright © 2016 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
